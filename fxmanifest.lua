80c7f64922f04d506c4a9ddc591e03d8b90d28e9
solareon <solareon@users.noreply.github.com>
2024-06-15 13:25:23 +0000

    chore: bump manifest version to v1.0.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 
 description "Solareon Scratchcard"
 author "solareon."
-version '1.0.3'
+version '1.0.4'
 repository 'https://github.com/solareon/slrn_scratchcard'
 
 lua54 'yes'
